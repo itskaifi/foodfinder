bd0b139f94c3b7f141651cbaf66ce004518dab91
Tayyab Khan <mr.tayyabkhan@yahoo.com>
2016-01-31 17:09:20 +0530

    Added Restaurant class

---
+++ lib/restaurant.rb
@@ -0,0 +1,16 @@
+class Restaurant 
+	@@filepath = nil
+
+	def self.file_exist?
+		# class should know if the restaurant file exists
+	end
+
+	def self.create_file
+		# create the restaurant file		
+	end
+
+	def self.save_restaurant
+		# read the restaurant file
+		# return instance of restaurant
+	end
+end
